26f77363fcea3419d681389ad7489328b6391465
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-09-20 01:19:10 +0500

    User Dashboard with Firebase Auth

---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { AuthProvider } from "@/context/AuthContext";
+import Navbar from "@/components/Navbar";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Firebase Auth App",
+  description: "Next.js + Firebase Authentication",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <AuthProvider>
+          <Navbar />
+          <main className="container">{children}</main>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
